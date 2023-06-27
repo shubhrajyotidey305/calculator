4dc2dda201e0c2560e656967e49f41786bb75190
Shubhrajyoti Dey <shubhrajyotidey305@gmail.com>
2023-06-27 20:34:16 +0530

    percentage added

--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -1,6 +1,7 @@
 import 'package:calculator_redoq/Utilities/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
+import 'package:basic_utils/basic_utils.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -35,9 +36,23 @@ class _HomeScreenState extends State<HomeScreen> {
         }
       } else if (buttonText == "=") {
         expression = equation;
+        for (int i = 0; i < expression.length; i++) {
+          if (expression[i] == '%') {
+            int left = i - 1;
+            int right = i + 1;
+            while (left >= 0 && StringUtils.isDigit(expression[left])) {
+              left--;
+            }
+            left++;
+            right++;
+            expression = StringUtils.addCharAtPosition(expression, '(', left);
+            expression = StringUtils.addCharAtPosition(expression, ')', right);
+            i += 2;
+          }
+        }
         expression = expression.replaceAll('×', '*');
         expression = expression.replaceAll('÷', '/');
-        expression = expression.replaceAll('%', '/100');
+        expression = expression.replaceAll('%', '*0.01');
 
         try {
           Parser p = Parser();
@@ -77,7 +92,7 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Text(
             buttonText,
             style: const TextStyle(
-                fontSize: 30.0,
+                fontSize: 20,
                 fontWeight: FontWeight.normal,
                 color: Colors.white),
           ),
@@ -90,77 +105,92 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          Container(
-            alignment: Alignment.centerRight,
-            padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              controller: _scrollController,
-              child: Text(
-                equation,
-                style: const TextStyle(fontSize: 35, color: Colors.white),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      body: SizedBox(
+        height: Config(context).height,
+        width: Config(context).width,
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                controller: _scrollController,
+                child: Text(
+                  equation,
+                  style: const TextStyle(fontSize: 35, color: Colors.white),
+                ),
               ),
             ),
-          ),
-          Container(
-            alignment: Alignment.centerRight,
-            padding: const EdgeInsets.fromLTRB(10, 30, 10, 0),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Text(
-                result,
-                style: const TextStyle(fontSize: 40, color: Colors.white),
+            Container(
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Text(
+                  result,
+                  style: const TextStyle(fontSize: 40, color: Colors.white),
+                ),
               ),
             ),
-          ),
-          Divider(thickness: 2, color: grey1),
-          Expanded(
-            flex: 5,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Expanded(child: customButton("C", grey1)),
-                    Expanded(child: customButton("7", grey2)),
-                    Expanded(child: customButton("4", grey2)),
-                    Expanded(child: customButton("1", grey2)),
-                    Expanded(child: customButton(".", grey2)),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Expanded(child: customButton("DEL", grey1)),
-                    Expanded(child: customButton("9", grey2)),
-                    Expanded(child: customButton("5", grey2)),
-                    Expanded(child: customButton("2", grey2)),
-                    Expanded(child: customButton("0", grey2)),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Expanded(child: customButton("%", grey1)),
-                    Expanded(child: customButton("8", grey2)),
-                    Expanded(child: customButton("6", grey2)),
-                    Expanded(child: customButton("3", grey2)),
-                    Expanded(child: customButton("00", grey2)),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Expanded(child: customButton("×", purple1)),
-                    Expanded(child: customButton("÷", purple1)),
-                    Expanded(child: customButton("-", purple1)),
-                    Expanded(child: customButton("+", purple1)),
-                    Expanded(child: customButton("=", purple2)),
-                  ],
-                ),
-              ],
-            ),
-          )
-        ],
+            Divider(thickness: 2, color: grey1),
+            Expanded(
+              flex: 5,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Expanded(child: customButton("C", grey1)),
+                        Expanded(child: customButton("7", grey2)),
+                        Expanded(child: customButton("4", grey2)),
+                        Expanded(child: customButton("1", grey2)),
+                        Expanded(child: customButton(".", grey2)),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Expanded(child: customButton("DEL", grey1)),
+                        Expanded(child: customButton("9", grey2)),
+                        Expanded(child: customButton("5", grey2)),
+                        Expanded(child: customButton("2", grey2)),
+                        Expanded(child: customButton("0", grey2)),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Expanded(child: customButton("%", grey1)),
+                        Expanded(child: customButton("8", grey2)),
+                        Expanded(child: customButton("6", grey2)),
+                        Expanded(child: customButton("3", grey2)),
+                        Expanded(child: customButton("00", grey2)),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Expanded(child: customButton("×", purple1)),
+                        Expanded(child: customButton("÷", purple1)),
+                        Expanded(child: customButton("-", purple1)),
+                        Expanded(child: customButton("+", purple1)),
+                        Expanded(child: customButton("=", purple2)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
